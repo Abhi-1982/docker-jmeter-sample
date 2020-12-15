2a04a21304269d47e098d45b25b092067c3055f5
Abhimanyu Singhal <abhimanyu.singhal@live.com>
2020-12-15 18:17:24 +0530

    fixe quotation

--- exec-jmeter.sh
+++ exec-jmeter.sh
@@ -3,5 +3,5 @@
 for filename in /testplans/*.jmx; do
     NAME=$(basename $filename)
     NAME="${NAME%.*}"
-    jmeter -n -t /testplans/$filename -l /testresults/$NAME.jtl"
+    jmeter -n -t /testplans/$filename -l /testresults/$NAME.jtl
 done
